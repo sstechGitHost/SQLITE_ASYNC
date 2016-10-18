52c09b9cc72ff5ea66795bb386022611c0f5e262
Nahidahmed <nahid.mansuri@gmail.com>
2016-10-18 08:22:04 +0530

    SQLite Async Sample

---
+++ People/People/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using People.Models;
+using System;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+
+namespace People
+{
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();            
+        }
+
+        public async void OnNewButtonClicked(object sender, EventArgs args)
+        {
+            statusMessage.Text = "";
+                        
+            await App.PersonRepo.AddNewPerson(newPerson.Text);
+            statusMessage.Text = App.PersonRepo.StatusMessage;
+        }
+        
+        public async void OnGetButtonClicked(object sender, EventArgs args)
+        {
+            statusMessage.Text = "";
+
+            ObservableCollection<Person> people = new ObservableCollection<Person>(await App.PersonRepo.GetAllPeople());
+            peopleList.ItemsSource = people;
+        }
+    }
+}
